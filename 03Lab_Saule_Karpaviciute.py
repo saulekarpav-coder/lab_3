00c1c9221e006a59e49c075c51d114f4800b074c
Saule <saulekarpav@gmail.com>
2026-04-28 23:11:28 +0300

    pilnas kodas

--- 03Lab_Saule_Karpaviciute.py
+++ 03Lab_Saule_Karpaviciute.py
@@ -1,7 +1,17 @@
+import os
+
 import pandas as pd 
-df = pd.read_csv("athlete_events.csv").sample(n=50_000, 
-random_state=42) 
+
+
+base_dir = os.path.dirname(__file__)
+file_path = os.path.join(base_dir, "duomenys", "athlete_events.csv")
+
+df = pd.read_csv(file_path).sample(n=50_000, random_state=42)
+
+base_dir = os.path.dirname(__file__)
+file_path = os.path.join(base_dir, "athlete_events.csv")
 df.to_csv("athlete_events.csv", index=False)
+
 
 import time
 import random
